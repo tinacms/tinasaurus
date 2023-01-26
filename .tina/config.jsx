0a6fa79fc7e2920774f51e06b700d25932472259
Scott Byrne <scott.byrne@forestry.io>
2023-01-26 14:52:57 -0400

    warning

--- .tina/config.jsx
+++ .tina/config.jsx
@@ -14,6 +14,38 @@ const branch =
   process.env.HEAD ||
   "main";
 
+const WarningIcon = (props) => {
+  return (
+    <svg
+      stroke="currentColor"
+      fill="currentColor"
+      stroke-width="0"
+      viewBox="0 0 24 24"
+      height="1em"
+      width="1em"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <path d="M11.001 10h2v5h-2zM11 16h2v2h-2z"></path>
+      <path d="M13.768 4.2C13.42 3.545 12.742 3.138 12 3.138s-1.42.407-1.768 1.063L2.894 18.064a1.986 1.986 0 0 0 .054 1.968A1.984 1.984 0 0 0 4.661 21h14.678c.708 0 1.349-.362 1.714-.968a1.989 1.989 0 0 0 .054-1.968L13.768 4.2zM4.661 19 12 5.137 19.344 19H4.661z"></path>
+    </svg>
+  );
+};
+
+const RestartWarning = () => {
+  return (
+    <p className="rounded-lg border shadow-sm px-4 py-2.5 bg-gradient-to-r from-yellow-50 to-yellow-100 border-yellow-200 mb-4">
+      <div className="flex items-center gap-2">
+        <WarningIcon className={`w-6 h-auto flex-shrink-0 text-yellow-400`} />
+        <div className={`flex-1 text-sm text-yellow-700 whitespace-normal	`}>
+          To see settings changes reflected on your site, you must restart the
+          Tina CLI after saving changes <em>(local development only)</em>.
+        </div>
+      </div>
+    </p>
+  );
+};
+
 const PostCollection = {
   name: "post",
   label: "Posts",
@@ -328,6 +360,15 @@ const SidebarCollection = {
   fields: [
     {
       type: "string",
+      name: "_warning",
+      ui: {
+        component: () => {
+          return <RestartWarning />;
+        },
+      },
+    },
+    {
+      type: "string",
       label: "Label",
       name: "label",
       required: true,
@@ -495,6 +536,15 @@ const SettingsCollection = {
     },
   },
   fields: [
+    {
+      type: "string",
+      name: "_warning",
+      ui: {
+        component: () => {
+          return <RestartWarning />;
+        },
+      },
+    },
     {
       type: "string",
       label: "Label",
@@ -701,6 +751,15 @@ const HomepageCollection = {
     },
   },
   fields: [
+    {
+      type: "string",
+      name: "_warning",
+      ui: {
+        component: () => {
+          return <RestartWarning />;
+        },
+      },
+    },
     {
       type: "string",
       label: "Label",
