f76eb501ab5ce58520428e18121fc4014bc6d3c6
James O'Halloran <james.p.ohalloran@gmail.com>
2023-09-11 12:30:20 -0300

    Switch to env

--- tina/config.jsx
+++ tina/config.jsx
@@ -820,8 +820,8 @@ const PagesCollection = {
 
 export default defineConfig({
   branch,
-  clientId: "ec80bfa2-69ad-4167-af8a-964c9609c8bf", // Get this from tina.io
-  token: "dd362ccf58b166176f0b818999c405cd90b8d1fb", // Get this from tina.io
+  clientId: process.env.NEXT_PUBLIC_TINA_CLIENT_ID, // Get this from tina.io
+  token: process.env.TINA_TOKEN, // Get this from tina.io
   build: {
     outputFolder: "admin",
     publicFolder: "static",
